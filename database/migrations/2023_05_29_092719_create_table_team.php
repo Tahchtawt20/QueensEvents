a52d0705541c155bacc7895a285fd316698adb7c
BoudadNadia <boudadn6@gmail.com>
2023-05-29 10:35:14 +0100

    add column bd

---
+++ database/migrations/2023_05_29_092719_create_table_team.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('team', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->unsignedBigInteger('id_typeT');
+            $table->foreign('id_typeT')->references('id')->on('type_team')->onUpdate('cascade')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('table_team');
+    }
+};
