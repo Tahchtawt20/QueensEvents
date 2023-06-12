49065e8b768730c6ce5de7c308ef51eba2683c0f
salmaAgour <salmaag983@gmail.com>
2023-06-12 10:32:27 +0100

    add new translations

--- lang/fr/messages.php
+++ lang/fr/messages.php
@@ -1,5 +1,13 @@
 <?php 
 return[
     'birthday'=>'Anniversaire',
-    'home'=>'Acceuil'
+    'home'=>'Acceuil',
+    'about'=>'À propos de nous',
+    'special'=>'Plats Spéciaux',
+    'theme'=>'Thèmes',
+    'team'=>'Équipes',
+    'title1'=>'Planificateur de mariage',
+    'title2'=>'& concepteur d\'événements',
+    'login'=>'Connexion',
+    'lang'
 ];
