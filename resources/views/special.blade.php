a15b1da52b0a923603fcdba465887a325c06cbc7
BoudadNadia <boudadn6@gmail.com>
2023-06-18 15:30:03 +0100

    special dishes next

--- resources/views/special.blade.php
+++ resources/views/special.blade.php
@@ -22,9 +22,6 @@
                         <a href="#" class="btn-primary mt-3">book a table</a>
                     </div>
                 </div>
-                {{-- <div class="col-lg-5 offset-lg-2 col-md-6 align-self-center mt-4 mt-md-0">
-                    <img src="{{ asset('pic5.jpg') }}" alt="" class="img-fluid shadow w-100">
-                </div> --}}
                 <div class="col-lg-6 hero-right">
                     <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
                         <div class="carousel-inner">
@@ -38,19 +35,10 @@
                                 <img class="d-block w-100" src="{{ asset('pic200.jpg') }}" alt="Third slide">
                             </div>
                             <div class="carousel-item">
-                                <img class="d-block w-100" src="{{ asset('pic300.jpg') }}" alt="Fourth slide">
+                                <img class="d-block w-100" src="{{ asset('pic800.jpg') }}" alt="Fourth slide">
                             </div>
                             <div class="carousel-item">
-                                <img class="d-block w-100" src="{{ asset('pic400.jpg') }}" alt="Fifth slide">
-                            </div>
-                            <div class="carousel-item">
-                                <img class="d-block w-100" src="{{ asset('pic500.jpg') }}" alt="Sixth slide">
-                            </div>
-                            <div class="carousel-item">
-                                <img class="d-block w-100" src="{{ asset('pic600.jpg') }}" alt="Seventh slide">
-                            </div>
-                            <div class="carousel-item">
-                                <img class="d-block w-100" src="{{ asset('pic700.jpg') }}" alt="Eighth slide">
+                                <img class="d-block w-100" src="{{ asset('pic300.jpg') }}" alt="Fifth slide">
                             </div>
 
                         </div>
@@ -74,21 +62,12 @@
                             <img class="img-fluid" src="{{ asset('pic200.jpg') }}" alt="">
                         </div>
                         <div class="item">
+                            <img class="img-fluid" src="{{ asset('pic800.jpg') }}" alt="">
+                        </div>
+                        <div class="item">
                             <img class="img-fluid" src="{{ asset('pic300.jpg') }}" alt="">
                         </div>
-                        <div class="item">
-                            <img class="img-fluid" src="{{ asset('pic400.jpg') }}" alt="">
-                        </div>
-                        <div class="item">
-                            <img class="img-fluid" src="{{ asset('pic500.jpg') }}" alt="">
-                        </div>
-                        <div class="item">
-                            <img class="img-fluid" src="{{ asset('pic600.jpg') }}" alt="">
-                        </div>
-                        <div class="item">
-                            <img class="img-fluid" src="{{ asset('pic700.jpg') }}" alt="">
-                        </div>
-                       
+
 
                     </div>
                 </div>
@@ -96,20 +75,37 @@
 
 
 
-
-
-
-
-
-
-
-
-
+          
 
             <div class="row mt-5">
                 <div class="col-lg-5 col-md-6 align-self-center order-2 order-md-1 mt-4 mt-md-0">
-                    <img src="{{ asset('pic10.jpg') }}" alt="" class="img-fluid shadow w-100">
+                    <img src="{{ asset('pic10.jpg') }}" alt="Fifth slide" class="img-fluid shadow w-100">
                 </div>
+                <div class="col-lg-5 col-md-6 align-self-center order-2 order-md-1 mt-4 mt-md-0">
+                    <img src="{{ asset('pic11.jpg') }}" alt="secound slide" class="img-fluid shadow w-100">
+                </div>
+                <div class="col-lg-5 col-md-6 align-self-center order-2 order-md-1 mt-4 mt-md-0">
+                    <img src="{{ asset('pic12.jpg') }}" alt="thired slide" class="img-fluid shadow w-100">
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+                <div class="owl-carousel owl-theme hero-carousel">
+                    <div class="item">
+                        <img class="img-fluid" src="{{ asset('pic10.jpg') }}" alt="">
+                    </div>
+                     <div class="item">
+                        <img class="img-fluid" src="{{ asset('pic11.jpg') }}" alt="">
+                    </div>
+                    <div class="item">
+                        <img class="img-fluid" src="{{ asset('pic12.jpg') }}" alt="">
+                    </div>
+
                 <div class="col-lg-5 offset-lg-2 col-md-6 align-self-center order-1 order-md-2 py-5">
                     <h2 class="special-number">02.</h2>
                     <div class="dishes-text">
@@ -123,11 +119,31 @@
                     </div>
                 </div>
             </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             <div class="row mt-5">
                 <div class="col-lg-5 col-md-6 align-self-center py-5">
                     <h2 class="special-number">03.</h2>
                     <div class="dishes-text">
-                        <h3><span>Beef</span><br> Steak Sauce</h3>
+                        <h3><span>Beef</span><br> jus </h3>
                         <p class="pt-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, ea
                             vero alias perferendis quas animi doloribus voluptates. Atque explicabo ea nesciunt
                             provident libero qui eum, corporis esse quos excepturi soluta?</p>
@@ -135,10 +151,47 @@
                         <a href="#" class="btn-primary mt-3">book a table</a>
                     </div>
                 </div>
-                <div class="col-lg-5 offset-lg-2 col-md-6 align-self-center mt-4 mt-md-0">
-                    <img src="{{ asset('pic14.jpg') }}" alt="" class="img-fluid shadow w-100">
+                <div class="col-lg-6 hero-right">
+                    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img class="d-block w-100" src="{{ asset('pic234.jpg') }}" alt="First slide">
+                            </div>
+                            <div class="carousel-item">
+                                <img class="d-block w-100" src="{{ asset('pic456.jpg') }}" alt="Second slide">
+                            </div>
+                            <div class="carousel-item">
+                                <img class="d-block w-100" src="{{ asset('pic123.jpg') }}" alt="Third slide">
+                            </div>
+
+                        </div>
+                        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>
+                    <div class="owl-carousel owl-theme hero-carousel">
+                        <div class="item">
+                            <img class="img-fluid" src="{{ asset('pic234.jpg') }}" alt="">
+                        </div>
+                        <div class="item">
+                            <img class="img-fluid" src="{{ asset('pic456.jpg') }}" alt="">
+                        </div>
+                        <div class="item">
+                            <img class="img-fluid" src="{{ asset('pic123.jpg') }}" alt="">
+                        </div>
+                    </div>
                 </div>
             </div>
+
+
+
+
+
         </div>
     </div>
 </section>
